56e87e801dd36d0f432002876514583266bc0132
Paul-M92 <mdlulipaul@yahoo.com>
2017-05-16 23:57:00 +0200

    Final user module sql script

--- SQL sripts UserModule.sql
+++ SQL sripts UserModule.sql
@@ -1,6 +1,6 @@
 
 -- Database Creation
-----------------------------------------------------------------------------------------------------------------------------------------------------------
+-- --------------------------------------------------------------------------------------------------------------------------------------------------------
 CREATE DATABASE "COS730UserModule"
     WITH 
     OWNER = postgres
@@ -15,7 +15,7 @@ COMMENT ON SCHEMA "UserModule"
     IS 'This schema contains a collection of objects relating to the user module implementaion';
 
 -- User Table Creation
-----------------------------------------------------------------------------------------------------------------------------------------------------------
+-- --------------------------------------------------------------------------------------------------------------------------------------------------------
 	
 CREATE TABLE user_module.nav_user
 (
@@ -45,9 +45,68 @@ ALTER TABLE user_module.nav_user
     OWNER to postgres;
 	
 CREATE INDEX nav_user_email_idx ON user_module.nav_user (email);
-	
+
+-- User Roles Table Creation
+-- -------------------------------------------------------------------------------------------
+
+CREATE TABLE user_module.nav_user_role
+(
+    username character varying(50) NOT NULL,
+    role character varying(20) NOT NULL DEFAULT 'USER',
+    PRIMARY KEY (username),
+    CONSTRAINT fk_username FOREIGN KEY (username)
+        REFERENCES user_module.nav_user (username) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE user_module.nav_user_role
+    OWNER to postgres;
+
+-- Trigger Statements
+-- --------------------------------------------------------------------------------------------------------------------------------------------------------	
+
+-- On Insert Trigger Statement
+CREATE FUNCTION trigger_insert_nav_user_roles_function()
+RETURNS trigger AS $$
+BEGIN
+  IF (select nav_user.is_admin from user_module.nav_user where nav_user.username = NEW.username) THEN
+  	INSERT INTO user_module.nav_user_role(username, role) VALUES (NEW.username, 'ADMIN');
+  ELSE
+  	INSERT INTO user_module.nav_user_role(username) VALUES (NEW.username);
+  END IF;
+  RETURN NEW;
+END $$ LANGUAGE 'plpgsql';
+    
+CREATE TRIGGER trigger_insert_nav_user_roles
+AFTER INSERT ON user_module.nav_user
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_insert_nav_user_roles_function();
+
+
+-- On Update Trigger Statement
+CREATE FUNCTION trigger_update_nav_user_roles_function()
+RETURNS trigger AS $$
+BEGIN
+  IF (select nav_user.is_admin from user_module.nav_user where nav_user.username = NEW.username) THEN
+  	UPDATE user_module.nav_user_role SET role = 'ADMIN' WHERE nav_user_role.username = NEW.username;
+  ELSE
+  	UPDATE user_module.nav_user_role SET role = 'USER' WHERE nav_user_role.username = NEW.username;
+  END IF;
+  RETURN NEW;
+END $$ LANGUAGE 'plpgsql';
+    
+CREATE TRIGGER trigger_update_nav_user_roles
+AFTER UPDATE ON user_module.nav_user
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_update_nav_user_roles_function();
+-- --------------------------------------------------------------------------------------------------------------------------------------------------------	
+
 -- Insert Statements
-----------------------------------------------------------------------------------------------------------------------------------------------------------
+-- --------------------------------------------------------------------------------------------------------------------------------------------------------
 
 INSERT INTO user_module.nav_user (username,  password, firstname, lastname , email, cell_number, mac_address, activated, is_admin)
 VALUES ('Admin', crypt('P@ssW0rd', gen_salt('bf',8)) ,'AdminName' ,'AdminLastName', 'admin@gmail.com', '+27711640514','FD-C3-65-2D-DD-25', true, true);
@@ -60,17 +119,7 @@ INSERT INTO user_module.nav_user (username, password, firstname, lastname, email
 VALUES ('Stevo', crypt('StevoPass', gen_salt('bf',8)),'Steve' ,'Nash', 'nash@gmail.com', '+27700640504', '40-83-D4-10-B1-99', true, false);
 INSERT INTO user_module.nav_user (username, password, firstname, lastname, email, cell_number, mac_address, activated, is_admin)
 VALUES ('ChrisRk', crypt('ChrisPass', gen_salt('bf',8)),'Chris' ,'Brown', 'brown@gmail.com', '+27611440316', '7D-26-62-03-1E-F2', true, false);
-----------------------------------------------------------------------------------------------------------------------------------------------------------
+-- --------------------------------------------------------------------------------------------------------------------------------------------------------
 
+select * from user_module.nav_user_role;
 select * from user_module.nav_user;
-
----------------------------------------------------------------------------------------------
-{
-	"username":"testguy",
-	"password":"testpass",
-	"firstname":"test",
-	"lastname":"guy",
-	"email":"guy@gmail.com",
-	"cell_number":"+27788670914",
-	"mac_address":"06-35-B1-5A-B1-63"
-}
